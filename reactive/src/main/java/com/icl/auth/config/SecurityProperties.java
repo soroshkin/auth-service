1680fa5de607e6862ff47a0099d0b742a6c716aa
soroshkin <soroshkin_a_m@mail.ru>
2020-07-28 14:09:00 +0300

    Change get security properties from environment variables

--- reactive/src/main/java/com/icl/auth/config/SecurityProperties.java
+++ reactive/src/main/java/com/icl/auth/config/SecurityProperties.java
@@ -3,10 +3,8 @@ package com.icl.auth.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "classpath:/security-settings.yml")
 @EnableConfigurationProperties
 @ConfigurationProperties
 public class SecurityProperties {
